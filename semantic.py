7286b17209f85d561f0a774e63ecf484f9e0068d
InfamousLeather <120725146+InfamousLeather@users.noreply.github.com>
2022-12-20 11:25:37 +0000

    Changes to semantic.py
    Additional comments added for the project file

--- semantic.py
+++ semantic.py
@@ -1,28 +1,38 @@
+# Task 38 Compulsory Task 1
+
+# This program will serve as an example for the similarity comparison method included in the SpaCy module:
+
+# Import statement and shortcut name variable for 'en_core_web_sm' method:
 import spacy
 nlp = spacy.load('en_core_web_sm')
 
-# word1 = nlp("cat")
-# word2 = nlp("monkey")
-# word3 = nlp("banana")
+# Variable names given to example words after applying our nlp method:
+word1 = nlp("cat")
+word2 = nlp("monkey")
+word3 = nlp("banana")
 
-# print(word1.similarity(word2))
-# print(word3.similarity(word2))
-# print(word3.similarity(word1))
+# print statement for similarities between 2 words:
+print(word1.similarity(word2))
+print(word3.similarity(word2))
+print(word3.similarity(word1))
 
 # The output from the above code was generally expected, however I did find it interesting to see how specifically it
 # calculates the similarity.  It would be very interesting to find out exactly how it draws this comparison and makes
 # a decision to such a precise mathematical figure.
 
-#tokens = nlp('cat dog apple monkey tree fish banana')
+# Series of words assigned as a string named tokens:
+tokens = nlp('cat dog apple monkey tree fish banana')
 
-#for token1 in tokens:
-#    for token2 in tokens:
-#        print(token1.text, token2.text, token1.similarity(token2))
+# Nested For Loops used to iterate through our string, comparing each word with each other word:
+for token1 in tokens:
+    for token2 in tokens:
+        print(token1.text, token2.text, token1.similarity(token2))
 
 # When introducing more tokens, it was interesting to see some of the calculations were perhaps different that I'd
 # expected. Namely I had anticipated tree having a slightly higher similarity with apple, and potentially with monkey,
 # given that apples grow on trees and monkeys often live in them.
 
+# Second string using my own example words:
 tokens_new = nlp('guitar microphone gig wombat butterfly metallica')
 
 for token1 in tokens_new:
@@ -36,7 +46,8 @@ for token1 in tokens_new:
 # butterfly had relatively strong similarities.
 
 # The pdf did not make it clear whether I should run the included example file with the simpler 'en_core_web_sm' model
-# or to use it on this file, which would seemingly make more sense for a direct comparison as the two files do
-# different things.  Regardless, it appears that en_core_web_sm has no word vectors loaded with it, and so the
-# similarity between words is based on other factors.  Ironically the simpler model did actually return stronger
-# correlations between tokens like gig/metallica compared with the _md model.
+# or to use it on this file, but I tried both and found that, with the data on the example file, was very difficult to
+# draw a comparison between the two, given the lack of labels on the results.  Regardless, it appears that
+# en_core_web_sm has no word vectors loaded with it, and so the similarity between words is based on other factors.
+# Ironically the simpler model, when run on this file, did actually return stronger correlations between tokens like
+# gig/metallica compared with the _md model.
